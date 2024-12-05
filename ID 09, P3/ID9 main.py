30e4d8acfbe326b43ad0db43f992244b7d22730f
Nicolai Martini <152643549+NicolaiMartini@users.noreply.github.com>
2024-12-05 16:17:04 +0100

    Thingsboard RPC introduced & hardware pins updated
    Need more work.

--- ID 09, P3/ID9 main.py
+++ ID 09, P3/ID9 main.py
@@ -13,54 +13,89 @@ from hardware_pins import pin_buzzer
 from uthingsboard.client import TBDeviceMqttClient
 from mpu6050 import MPU6050
 from neopixel import NeoPixel
+import gc
+import secrets
+from sys import exit 
+
 
 ##### PINS
 buzzer=pin_buzzer
 i2c = I2C(0) #I2C init. Pin 18 SCL, Pin 19 SDA
 neopixel_ring=15
 
+
 ##### CONFIGURATIONS
-n = 12 #Number of NeoPixels
+n = 3 #Number of NeoPixels
 alarm_triggered=False
+alarm_enabled=False
+
 
 ##### OBJECTS
 buzzer_PWM_objekt=PWM(Pin(buzzer,Pin.OUT),freq=1,duty=850)
 imu = MPU6050(i2c) #MPU
 np = NeoPixel(Pin(neopixel_ring, Pin.OUT), n) #np Object on Pin 16
+client=TBDeviceMqttClient(secrets.SERVER_IP_ADDRESS,
+                          access_token=secrets.ACCESS_TOKEN)
+
 
 ##### FUNCTIONS
-def buzzer_alarm_loud(): # Buzzer alarm funktion
+def disable_active_alarm():
+    neopixel_clear()
+    buzzer_PWM_objekt.duty(0)
+
+def neopixel_flash(r,g,b):
+    for i in range(n):
+        np[i]=(r,g,b)
+    np.write()
+
+def neopixel_clear():
+    for i in range(n):
+        np[i]=(0,0,0)
+    np.write()
+
+def trigger_alarm(r, g, b): #Farvefunktion til NeoPixels
+    neopixel_flash(r,g,b)
     buzzer_PWM_objekt.freq(1023)
     buzzer_PWM_objekt.duty(512)
-def buzzer_alarm_quiet():
+    sleep(0.5)
+    neopixel_flash(b,g,r)
     buzzer_PWM_objekt.freq(512)
-    buzzer_PWM_objekt.duty(512)
-    
-    
-def trigger_alarm(r, g, b): #Farvefunktion til NeoPixels
-    for i in range(n):
-        np[i] = (r, g, b)
-    np.write()
-    buzzer_alarm_loud()
+    buzzer_PWM_objekt.freq(512)
     sleep(0.5)
-    for i in range(n):
-        np[i]=(r,r,r)
-    np.write()
-    buzzer_alarm_quiet()
-    sleep(0.5)
+
+def hanlder(req_id, method, params):
+    """Handler callback to receive RPC from server"""
+    print(f"Response: {req_id}: {method}, params {params}")
+    print(params, "params type:", type(params))
+    try:
+        if method=="Enable alarm":
+            if params==True:
+                print("Alarm enabled")
+                global alarm_enabled=True
+            elif params==False:
+                print("Alarm disabled")
+                global alarm_enabled=False
+    except TypeError as e:
+        print(e)
+        
+client.connect()
 
 
 ##### PROGRAM
 while True:
-    values = imu.get_values() #Henter værdier fra MPU
-    if  values["acceleration x"] < -1000: # Tænder for bremselys, hvis over -1000
-        alarm_triggered=True
-    elif values["acceleration x"] > -1000:
+    if alarm_enabled==True:
+
+        values = imu.get_values() #Henter værdier fra MPU
+        if  values["acceleration x"] < -1000: # Tænd for alarm hvis 
+            alarm_triggered=True
+        if values["acceleration x"] > -1000: # Deaktiver lys og lyd
+            alarm_triggered=False
+
+        if alarm_triggered==False:
+            disable_active_alarm()
+        if alarm_triggered==True:
+            trigger_alarm(255,0,0) #Funktion kaldes
+
+    if alarm_enabled==False:
         alarm_triggered=False
-    if alarm_triggered==False:
-        for i in range(n):
-            np[i]=(0,0,0)
-        np.write()
-        buzzer_PWM_objekt.duty(0)
-    if alarm_triggered==True:
-        trigger_alarm(255,0,0) #Funktion kaldes
+        disable_active_alarm()
---
+++ hardware_pins_config.py
@@ -0,0 +1,36 @@
+"""
+Denne fil er til vores hardware og hvilke pins de er tilsluttet,
+så der er et samlet overblik.
+Så kan vi evt. også importere vores pins herfra.
+"""
+
+dht11_pin = 15 #
+
+pin_buzzer = 14 # Jumpes JP1-SCK <-> JP6-GP6 eller via Port Exp.
+
+pin_adc_scl = 18
+pin_adc_sda = 19
+
+
+pin_adc = 39 # IKKE PIN 34!!! DEN ER TILKOBLET POTENTIOMETER!!!
+
+pin_blinker_left = 26
+pin_button_left = 4
+
+pin_blinker_right = 12 # Jumpes JP1-MISO <-> JP6-GP2 eller via Port Exp.
+pin_button_right = 0
+
+pin_lcd_rs        = 27
+pin_lcd_enable    = 25
+pin_lcd_db4       = 33
+pin_lcd_db5       = 32
+pin_lcd_db6       = 21
+pin_lcd_db7       = 22
+pin_lcd_contrast  = 23
+lcd_num_lines     =  4
+lcd_num_columns   = 20
+
+pin_gps_port = 2                               # ESP32 UART port, Educaboard ESP32 default UART port
+gps_speed = 9600                           # UART speed, defauls u-blox speed
+
+
