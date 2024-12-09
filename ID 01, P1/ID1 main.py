49834ef5eb080c221b82481f84ff9fb4e74f2366
Nicolai Martini <152643549+NicolaiMartini@users.noreply.github.com>
2024-12-09 11:04:08 +0100

    Major update to backend
    major rework of setup and internal components of backend.py

--- ID 01, P1/ID1 main.py
+++ ID 01, P1/ID1 main.py
@@ -8,18 +8,18 @@ Samtidig skal batteristatus, resterende kapacitet i procent, vises.
 from adc_sub import ADC_substitute # Skal erstattes af INA219 modul
 from ina219_lib import INA219
 from time import sleep
-import hardware_pins_config as hw
+import backend
 
 # CONFIGURATION
-pin_ina_scl=hw.pin_ina_scl # INA 219 serial clock line
-pin_adc_sda=hw.pin_ina_sda # INA 219 serial data line
+pin_ina_scl=backend.pin_ina_scl # INA 219 serial clock line
+pin_adc_sda=backend.pin_ina_sda # INA 219 serial data line
 
 
 # OBJECTS
 # Vil vi instantiere i filen?
 i2c=I2C(scl=Pin(pin_ina_scl),sda=Pin(pin_ina_sda),freq=400000) # I2C instantiering af ina-pins
 # Eller vil vi hente objektet fra anden fil?
-ina219=hw.ina219
+ina219=backend.ina219
 # ina219.set_calibration_16V_400mA() # Følsomhed, kan ændres
 
 
--- backend.py
+++ backend.py
@@ -1,107 +1,101 @@
 """
 Denne fil er til vores hardware og hvilke pins de er tilsluttet,
 så der er et samlet overblik.
-Så kan vi evt. også importere vores pins herfra.
+
+Der er også tilhørende configs og funktioner.
 """
+
+##### IMPORTS
 import dht
 
-from gpio_lcd import GpioLcd
 from machine import Pin, UART, I2C
-from gps_simple import GPS_SIMPLE
+from gpio_lcd import GpioLcd
 from ina219_lib import INA219
+from gps_simple import GPS_SIMPLE
 
 
-########## TEMPLATE
-#### NAVN
-# Pins
+##### PINS
+pin_button_two = 0
+pin_button_one = 4
+pin_gps_pps = 5 # Don't change
+pin_solenoid = 15
+pin_gps_tx = 16 # Don't change
+pin_gps_rx = 17 # Don't change
+pin_scl = 18 # I2C
+pin_sda = 19 # I2C
+pin_lcd_db6 = 21 # Don't change
+pin_lcd_db7 = 22 # Don't change
+pin_lcd_contrast = 23 # Don't change
+pin_lcd_enable = 25 # Don't change
+pin_neoring = 26
+pin_lcd_rs = 27 # Don't change
+pin_lcd_db5 = 32 # Don't change
+pin_lcd_db4 = 33 # Don't change
+pin_buzzer = 34
+pin_dht11 = 35
+pin_neostrip_one = 36
+pin_neostrip_two = 39
+pin_mpu = None # connect via sda and scl
+pin_ina = None # connect via sda and scl
 
 
-# Instantiering
+##### CONFIGURATIONS
+lcd_num_lines = 4
+lcd_num_columns = 20
+gps_baudrate = 9600 # u-block default
+gps_port = 2 # Don't change
 
 
-# Funktioner
+
+##### OBJECTS
+gps=GPS_SIMPLE(UART(gps_port,gps_baudrate)) # 5V
+lcd=GpioLcd(rs_pin=Pin(pin_lcd_rs),
+            enable_pin=Pin(pin_lcd_enable),
+            d4_pin=Pin(pin_lcd_db4),
+            d5_pin=Pin(pin_lcd_db5),
+            d6_pin=Pin(pin_lcd_db6),
+            d7_pin=Pin(pin_lcd_db7),
+            num_lines=lcd_num_lines,
+            num_columns=lcd_num_columns) # 3.3V
+
+ina=INA219(I2C(scl=Pin(pin_scl),sda=Pin(pin_sda),freq=400000)) # 3.3V
+
+dht11=dht.DHT11(Pin(pin_dht11)) # 3.3V
 
 
-
-#### dht11
-# Pins
-pin_dht11=None # MANGLER!
-
-# Instantiering
-# pin=Pin(pin_dht11) # 
-# dht11=dht.DHT11(pin)
-
-# Funktioner
+##### FUNCTIONS
 def dht11_temp():
-    """DHT11 measure temp in deg C,
-    as humidity is not needed."""
+    """Measure with dht11, return temp in °C"""
     dht11.measure()
     tempC=dht11.temperature()
     return tempC
 
+def ina_voltage():
+    """Read voltage from ina"""
+    return ina.get_bus_voltage()
 
-#### GPS
-# Pins
-## DO NOT MESS WITH GPS!
-pin_gps_port = 2 # ESP32 UART port, Educaboard ESP32 default UART port
-pin_gps_pps = 5
-pin_gps_tx = 16
-pin_gps_rx = 17
-gps_speed=9600
+def ina_current():
+    """Read current from ina"""
+    return ina.get_current()
 
-# Instantiering
-uart=UART(pin_gps_port,gps_speed) # Opsæt uart config til gps'en
-gps=GPS_SIMPLE(uart) # Opsæt GPS objektet
-
-
-#### LCD
-# Pins
-pin_lcd_rs        = 27
-pin_lcd_enable    = 25
-pin_lcd_db4       = 33
-pin_lcd_db5       = 32
-pin_lcd_db6       = 21
-pin_lcd_db7       = 22
-pin_lcd_contrast  = 23
-lcd_num_lines     =  4
-lcd_num_columns   = 20
-
-# Instantiation
-lcd=GpioLcd(
-    rs_pin=Pin(pin_lcd_rs),
-    enable_pin=Pin(pin_lcd_enable),
-    d4_pin=Pin(pin_lcd_db4),
-    d5_pin=Pin(pin_lcd_db5),
-    d6_pin=Pin(pin_lcd_db6),
-    d7_pin=Pin(pin_lcd_db7),
-    num_lines=lcd_num_lines,
-    num_columns=lcd_num_columns)
-
-
-# Funktioner
-def display(kol,linj,tekst):
-    """Indsæt linje, kolonne og tekst, for hvad der skal vises på display"""
-    if isinstance(tekst,str):
-        lcd.move_to(kol,linj)
-        lcd.putstr(" "*len(tekst))
-        lcd.move_to(kol,linj)
-        lcd.putstr(str(tekst))
-    else:
-        print("3rd input has to be a string.")
-
-#### INA219
-# Pins
-pin_ina_scl = 18 # SCL line
-pin_ina_sda = 19 # SDA line
-
-# Instantiering
-# i2c=I2C(scl=Pin(pin_ina_scl),sda=Pin(pin_ina_sda),freq=400000) # I2C instantiering af ina-pins
-# ina219=INA219(i2c) #  instantiering af ina219 med I2C
-
-# Funktioner
 def batt_percentage():
-    """adc through ina219, returns batt% from 3.0-4.2V
-    Li-ion batt."""
-    voltage=adc.read_voltage()*2 # *2 for a proper reading, *3 for simulated batteri
-    batt_percentage=((voltage-3)/(4.2-3.0))*100
+    """Return battery-%, 4.2V is cons. 100%, and 3.0V is considered 0%."""
+    voltage=ina_voltage()*2 # Multiply based on circuit voltage divider
+    batt_percentage=((voltage-3)/(4.2-3.0))*100 # Return is %-based
     return batt_percentage
+
+def display(col, lin, text):
+    """ Insert column, line and text for what to display on LCD."""
+    if isinstance(text,str):
+        """Check if input is a string"""
+        lcd.move_to(col,lin)
+        lcd.putstr(" "*len(text))
+        lcd.move_to(col,lin)
+        lcd.putstr(text)
+    else:
+        """Display only accepts strings"""
+        lcd.clear()
+        lcd.move_to(0,0)
+        lcd.putstr("Input is not string")
+        lcd.move_to(0,1)
+        lcd.putstr("Make input string")
