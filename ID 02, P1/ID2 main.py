f3f9964a083d407dd5f3049f66a93c26f9b1af57
Nicolai Martini <152643549+NicolaiMartini@users.noreply.github.com>
2024-12-17 09:57:09 +0100

    Rework of ID2
    ID2 should now be functional with and without GPS data.

--- ID 02, P1/ID2 main.py
+++ ID 02, P1/ID2 main.py
@@ -3,12 +3,13 @@ import gc
 
 from time import sleep
 from machine import reset
-import hardware_pins_config as hw
+import backend as hw
 
 
 ##### OBJECTS
 dht11=hw.dht11
 lcd=hw.lcd
+gps=hw.gps
 
 
 ##### PROGRAM
@@ -18,21 +19,34 @@ while True:
         if gc.mem_free() < 2000:          # free memory if below 2000 bytes left
             print("Garbage collected!")
             gc.collect()                  # free memory
-"""
+            """
 Raffiner hvilken data vi henter og gemmer hvornår
 """
-        if gps.receive_nmea_data() and gps.get_validity()=="A": # If gps data is received
-            data={"La":gps.get_latitude(),
-                  "Lo":gps.get_longitude(),
-                  "Course":gps.get_course(),
-                  "Speed":gps.get_speed(),
-                  "Temp":dht11_temp(),
-                  "Batt%":batt_percentage()}
-            hw.display(f"Batt%:{data.get('Batt%'):.0f} Temp:{data.get('Temp')}")
-            hw.display(f"La/Lo:{data.get('La'):.3f}/{data.get('Lo'):.3f}")
-            hw.display(f"Lo:{data.get('Lo'):.2f}")
-            # Lav Course om til at give kardinalværdier i stedet for tal
-            hw.display(f"Speed:{data.get('Speed'):.1f} Course:{data.get('Course')}")
+        if gps.receive_nmea_data(): # If gps data is received
+            if gps.get_validity()=="A":
+                data={"La":gps.get_latitude(),
+                      "Lo":gps.get_longitude(),
+                      "Course":gps.get_course(),
+                      "Speed":gps.get_speed(),
+                      "Temp":hw.dht11_temp(),
+                      "Batt%":hw.batt_percentage()}
+                hw.display(0,0,f"Batt%:{data.get('Batt%'):.0f} Temp:{data.get('Temp')}")
+                hw.display(0,1,f"La/Lo:{data.get('La'):.3f}/{data.get('Lo'):.3f}")
+                hw.display(0,2,f"Speed:{data.get('Lo'):.2f}")
+                # Lav Course om til at give kardinalværdier i stedet for tal
+                hw.display(0,3,f"Course:{data.get('Course')}")
+            if gps.get_validity()=="V":
+                data={"La":gps.get_latitude(),
+                      "Lo":gps.get_longitude(),
+                      "Course":gps.get_course(),
+                      "Speed":gps.get_speed(),
+                      "Temp":hw.dht11_temp(),
+                      "Batt%":hw.batt_percentage()}
+                hw.display(0,0,f"Batt%:{data.get('Batt%'):.0f} Temp:{data.get('Temp')}")
+                hw.display(0,1,f"La/Lo:N/A")
+                hw.display(0,2,f"Speed:N/A")
+                # Lav Course om til at give kardinalværdier i stedet for tal
+                hw.display(0,3,f"Course:N/A")
         sleep(1)                          # send telemetry once every second
     except KeyboardInterrupt:
         print("Disconnected!")
---
+++ Samlet løsning/ID2_main.py
@@ -0,0 +1,53 @@
+##### IMPORTS
+import gc
+
+from time import sleep
+from machine import reset
+import backend as hw #Omdøbt
+
+
+##### OBJECTS
+dht11=hw.dht11
+lcd=hw.lcd
+
+# Variabels
+printable=None
+
+##### PROGRAM
+def run():    
+    print(f"free memory: {gc.mem_free()}") # monitor memory left
+    if gc.mem_free() < 2000:          # free memory if below 2000 bytes left
+        print("Garbage collected!")
+        gc.collect()                  # free memory
+# Raffiner hvilken data vi henter og gemmer hvornår
+    if hw.gps.receive_nmea_data():
+        if hw.gps.get_validity()=="A": # If gps data is received
+            la = hw.gps.get_latitude()
+            lo = hw.gps.get_longitude()
+            course = hw.gps.get_course()
+            speed = hw.gps.get_speed()
+            printable=True
+        if hw.gps.get_validity()=="V":
+            la = "N/A"
+            lo = "N/A"
+            course = "N/A"
+            speed = "N/A"
+            printable=False
+        data={"La":la,
+                  "Lo":lo,
+                  "Course":course,
+                  "Speed":speed,
+                  "Temp":hw.dht11_temp(),
+                  "Batt%":hw.batt_percentage()}
+        if printable==True:
+            hw.display(0,  0, f"Bat%:{data.get('Batt%'):.1f}")
+            hw.display(10, 0, f"Temp:{data.get('Temp'):.1f}")
+            hw.display(0,  1, f"La/Lo:{float(data.get('La')):.3f}")
+            hw.display(0, 1, f"La:{float(data.get('Lo')):.3f}")
+            # Lav Course om til at give kardinalværdier i stedet for tal
+            hw.display(0, 2, f"Speed:{data.get('Speed'):.1f}")
+            hw.display(0, 3, f"Course:{data.get('Course')}")
+        if printable==False:
+            hw.display(0,  0, f"Bat%:{data.get('Batt%'):.1f}")
+            hw.display(10, 0, f"Temp:{data.get('Temp'):.1f}")
+            hw.display(0,  1, f"GPS unavailable")
