272951f20142e5c0b057f7e774c407d596fffa4e
HelligeGiffel <114979429+HelligeGiffel@users.noreply.github.com>
2024-12-17 11:06:41 +0100

    Update ID6 main.py

--- ID 06, P2/ID6 main.py
+++ ID 06, P2/ID6 main.py
@@ -4,7 +4,7 @@ from machine import Pin, I2C
 from neopixel import NeoPixel
 
 n = 12 #Number of NeoPixels
-np = NeoPixel(Pin(16, Pin.OUT), n) #np Object on Pin 16
+np = NeoPixel(Pin(2, Pin.OUT), n) #np Object on Pin 16
 
 i2c = I2C(0) #I2C init. Pin 18 SCL, Pin 19 SDA
 imu = MPU6050(i2c) #MPU
@@ -14,13 +14,11 @@ def set_color(r, g, b): #Farvefunktion til NeoPixels
         np[i] = (r, g, b)
     np.write()
     
-start_time_brakelight = ticks_ms() #Start time Nonblocking delay
-threshold_brakelight = 100 #Tærskel Nonblocking delay
-while True:
-    if ticks_ms() - start_time_brakelight > threshold_brakelight:
-        start_time_brakelight = ticks_ms()
-        values = imu.get_values() #Henter værdier fra MPU
-        if  values["acceleration x"] < -1000: # Tænder for bremselys, hvis over -1000
-            set_color(100,0,0) #Funktion kaldes
-        else:
-            set_color(0,0,0)   #Funktion kaldes
+def run:
+    values = imu.get_values() #Henter værdier fra MPU
+    if  values["acceleration y"] > 1000: # Tænder for bremselys, hvis over 1000
+        set_color(255,0,0) #Funktion kaldes
+        return 1000 #return mængden af tid bremselyset skal være tændt
+    else:
+        set_color(0,0,0)   #Funktion kaldes
+        
