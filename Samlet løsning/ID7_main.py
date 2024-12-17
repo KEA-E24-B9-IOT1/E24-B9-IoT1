0965ed9de79f6f22f89327cbe2cb511659c9d3a4
Nicolai Martini <152643549+NicolaiMartini@users.noreply.github.com>
2024-12-17 14:13:16 +0100

    ID 4 commenced, ID 7 done

---
+++ Samlet løsning/ID7_main.py
@@ -0,0 +1,26 @@
+import gc
+import secrets
+import backend
+from uthingsboard.client import TBDeviceMqttClient
+from time import ticks_ms
+
+
+ina=backend.ina
+
+
+average_current=[]
+
+
+def ina_current_average():
+    average_current.append(backend.ina_current())
+    if len(average_current)>36:
+        del average_current[0]
+    avg_current=sum(average_current)/len(average_current)
+    return avg_current
+
+def run():
+    battery_percentage=backend.batt_percentage()
+    battlife=3600*(backend.batt_percentage()/100)/ina_current_average()
+    telemetry = {'Battlife' : battlife}
+    return telemetry
+    
--- Samlet løsning/main.py
+++ Samlet løsning/main.py
@@ -4,27 +4,31 @@ import ID1_main
 import ID2_main
 import ID3_main
 import ID6_main
+import ID7_main
 
 
 from time import ticks_ms
 
 ##### Non blocking Delay Config #####
+
 id1 = ticks_ms()
 id2 = ticks_ms()
 id3 = ticks_ms()
 id4 = ticks_ms()
 id5 = ticks_ms()
 id6 = ticks_ms()
-
+id7 = ticks_ms()
 
 ##### Non Blocking Delay timing #####
 #Opgives i ms
+ina_meas = 1000
 id1timing = 120000
 id2timing = 5000
 id3timing = 5000
 # id4timing = None
 # id5timing = None
 id6timing = 500
+id7timing = 5000 # Skal være 300000
 
 
 ##### Startups
@@ -32,6 +36,7 @@ ID1_main.run()
 # Make client object to connect to thingsboard
 client = backend.TBDeviceMqttClient(secrets.SERVER_IP_ADDRESS, access_token = secrets.ACCESS_TOKEN)
 client.connect() # Connecting to ThingsBoard
+
 
 
 ##### Main Program
@@ -45,16 +50,20 @@ while True:
             ID2_main.run()
         if ticks_ms() > id3 + id3timing:
             id3 = ticks_ms()
-            telemetry=ID3_main.run()
-            client.send_telemetry(telemetry) #Sending telemetry  
-    #     if ticks_ms() > id4 + id4timing:
+            id3telemetry=ID3_main.run()
+            client.send_telemetry(id3telemetry) #Sending telemetry
+#         if ticks_ms() > id4 + id4timing:
 #             id4 = ticks_ms()
-    #         Pass
+#             ID4_main.run(ticks_ms())
     #     if ticks_ms() > id5 + id5timing:
     #         id5 = ticks_ms()
 #     Pass
         if ticks_ms() > id6 + id6timing:
             id6 = ticks_ms() + ID6_main.run() #Returnere positivt tal for at holde bremselyset tændt
+        if ticks_ms() > id7 + id7timing:
+            id7 = ticks_ms()
+            id7telemetry=ID7_main.run()
+            client.send_telemetry(id7telemetry)
     except KeyboardInterrupt:
         client.disconnect()               # Disconnecting from ThingsBoard
         reset()
