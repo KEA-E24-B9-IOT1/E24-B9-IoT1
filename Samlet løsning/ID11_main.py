5b1e091fe881bd57327a11fa8e58e091fa327149
HelligeGiffel <114979429+HelligeGiffel@users.noreply.github.com>
2024-12-19 16:06:05 +0100

    Updates to main and ID11
    Small changes and INA measuraments

--- Samlet løsning/ID11_main.py
+++ Samlet løsning/ID11_main.py
@@ -11,10 +11,38 @@ backend.color_short(backend.rb,0,0,0) # Start with turning off neopixel lights
 
 backend.color_short(backend.lb,0,0,0) # Start with turning off neopixel lights
 
+global leftcounter
+leftcounter = 0
 
+global rightcounter
+rightcounter = 0
+
+timer = ticks_ms()
+
+def blinker():
+    if ticks_ms() > timer + 500:
+        global leftcounter
+        global rightcounter
+        if leftcounter <= 0:
+            if leftcounter % 2 == 0: # True if Even, hvis der ikke er nogen rest efter modulo, så er tallet lige
+                backend.color_short(backend.lb,250,75,0) # Call backend-function to manipulate light
+            if leftcounter % 2 != 0: # False if Even, hvis der ikke er nogen rest efter modulo, så er tallet lige
+                backend.color_short(backend.lb,0,0,0) # Call backend-function to manipulate light
+            leftcounter -= 1
+        if rightcounter <= 0:
+            if rightcounter % 2 == 0:
+                backend.color_short(backend.rb,250,75,0)
+            if rightcounter % 2 != 0:
+                backend.color_short(backend.rb,0,0,0)
+            rightcounter -= 1
+        
 def left_blinker_func():
+    timer = ticks_ms()
+    global leftcounter
+    leftcounter = 6
     """Make left neopixel strip blink as a signal light"""
     print("Running left blinker") # For troubleshooting
+    '''
     timer=ticks_ms() # Non-blocking delay for manipulating light
     counter = 1 # Counter for on/off-cycle
     while True: # Mali's bedste ven
@@ -31,10 +59,14 @@ def left_blinker_func():
         if counter > 6: # End signal after 3 full on/off cycles
             print("Breaking left blinker") # For troubleshooting
             break
-
+'''
 def right_blinker_func():
+    timer = ticks_ms()
+    global rightcounter
+    rightcounter = 6
     """Everything from left_blinker_func applies heres as well"""
     print("Running right blinker")
+    '''
     timer=ticks_ms()
     counter=1
     while True:
@@ -51,3 +83,4 @@ def right_blinker_func():
         if counter > 6:
             print("Breaking left blinker")
             break
+'''
--- Samlet løsning/main.py
+++ Samlet løsning/main.py
@@ -10,7 +10,7 @@ import ID9_main
 import ID11_main
 
 
-from time import ticks_ms
+from time import ticks_ms,sleep
 
 ##### Non blocking Delay Config #####
 """A colleciton of timers for various functions"""
@@ -21,6 +21,9 @@ id4moving = ticks_ms()
 id4alarm = ticks_ms()
 id6 = ticks_ms()
 id7 = ticks_ms()
+id11 = ticks_ms()
+id11_2 = ticks_ms()
+ged = ticks_ms()
 
 
 ##### Non Blocking Delay timing #####
@@ -32,6 +35,9 @@ id4movingtiming = 1000 # How often to check ID 4 if the bike is moving
 id4alarmtiming= 180000 # How often to check ID 4 for alarm arming, if not armed
 id6timing = 500 # How often to check if ID 6 should be executed
 id7timing = 300000 # How often to execute ID 7 for estimated battery life
+id11timing = 100 # How often to update blinker Blinker
+id11timing2 = 1000 # How often to block buttons
+gedtiming = 1000
 
 
 ##### Startup configs
@@ -42,12 +48,10 @@ client.connect() # Connecting to ThingsBoard
 print("Connecting") # For troubleshooting
 ID4AlarmSystem = False # For ID 4 alarm system
 ID9AlarmSystem = False # For ID 9 alarm system
-Solenoid_Toggle = False # Solenoid active/inactive state
+ID11_main.id11_start()
 
-
-##### Functions
-def alarm_handler(req_id, method, params):
-    """Handler callback to receive RPC from server"""
+def handler(req_id, method, params):
+    """ Handler callback to receive RPC from server"""
     print(f"Response: {req_id}: {method}, params {params}")
     print(params, "params type:", type(params))
     try:
@@ -62,57 +66,46 @@ def alarm_handler(req_id, method, params):
     except TypeError as e:
         print(e)
 
-def solenoid_handler(req_id, method, params):
-    """Handler callback to receive RPC from server"""
-    print(f"Response: {req_id}: {method}, params {params}")
-    print(params, "params type:", type(params))
-    try:
-#         global Solenoid_Toggle
-        if method == "Solenoid state":
-            if params == True:
-                print("Solenoid enabled")
-#                 Solenoid_Toggle = True
-                backend.solenoid.value(1)
-            elif params == False:
-                print("Solenoid disabled")
-#                 Solenoid_Toggle = False
-                backend.solenoid.value(0)
-    except TypeError as e:
-        print(e)
-
-
-client.set_server_side_rpc_request_handler(alarm_handler) # Thingsboard-Alarm connection
-client.set_server_side_rpc_request_handler(solenoid_handler) # Thingsboard-Solenoid connection
-
 
 ##### Main Program
 while True:
     try:
+        client.set_server_side_rpc_request_handler(handler) # Keep checking for messages from ThingsBoard
         client.check_msg() # As above
+
+
+        """Following section is currently not working"""
+        '''if backend.left_button.value()==1 and backend.right_button.value()==1: # If both buttons are held down at the same time
+            print("Solenoid activated") # For troubleshooting
+            backend.solenoid.on() # Turn on solenoid to allow un-/locking
+            solenoid_timer=ticks_ms() # To make sure solenoid allows manipulation for 10 seconds
+            if ticks_ms()-solenoid_timer>10000: # Count-down to disable solenoid
+                solenoid.off() # Disable'''
+
         if ticks_ms() > id1 + id1timing: 
-            print("ID 1 running") # For troubleshooting
+            #print("ID 1 running") # For troubleshooting
             id1 = ticks_ms() # Reset non-blocking delay
             ID1_main.run() # Execute ID 1. See ID1_main.py for more info.
         if ticks_ms() > id2 + id2timing:
-            print("ID 2 running") # Display temp & GPS
+            #print("ID 2 running") # Display temp & GPS
             id2 = ticks_ms() # Resetter nonblocking delay timer
             ID2_main.run() # Execute ID2. See ID2_main.py for more info.
         if ticks_ms() > id3 + id3timing:
-            print("ID 3 running")
+            #print("ID 3 running")
             id3 = ticks_ms()
             id3telemetry=ID3_main.run() # Execute ID3 and save info for upload to Thingsboard. See ID3_main.py for more info.
             client.send_telemetry(id3telemetry) #Sending telemetry to Thingsboard
         if ticks_ms() > id4moving + id4movingtiming:
             if ID4AlarmSystem==False: # Only run following indentation if alarm isn't armed'
-                print("ID 4 is_moving running") # For troubleshooting
+                #print("ID 4 is_moving running") # For troubleshooting
                 id4moving = ticks_ms()
                 ID4_main.is_moving()
         if ticks_ms() > id4alarm + id4alarmtiming:
-            if ID4AlarmSystem==True: # Only run following indentation if alarm isn't armed
-                print("ID 4 alarm running") # Cykel stået stille i 3 min, aktiver alarm
+            if ID4AlarmSystem==False: # Only run following indentation if alarm isn't armed
+                #print("ID 4 alarm running") # Cykel stået stille i 3 min, aktiver alarm
                 id4alarm = ticks_ms()
                 ID4AlarmSystem=ID4_main.alarm()
-        if ID4AlarmSystem == True: # Only run following indentation if alarm is armed
+        if ID4AlarmSystem == True: # Only run following indentation is alarm is armed
             values=ID6_main.mpu.get_values() # Fetch values
             if values["acceleration y"] > 5000 or values["acceleration x"] > 5000: # Execute following indentation if bike is moved horizontally
                 print("ID 4 alarm triggered, sending gps data to Thingsboard") # For troubleshooting
@@ -120,10 +113,10 @@ while True:
                 client.send_telemetry(id4telemetry) # Send saved data to Thingsboard.
                 ID4AlarmSystem=False # Disable alarm as it's been triggered.
         if ticks_ms() > id6 + id6timing:
-            print("ID 6 running")
+            #print("ID 6 running")
             id6 = ticks_ms() + ID6_main.run() # Return positive number to extend brake light function.
         if ticks_ms() > id7 + id7timing:
-            print("ID 7 running")
+            #print("ID 7 running")
             id7 = ticks_ms()
             id7telemetry=ID7_main.est_batt_life() # Execute ID 7 and save estimated battery life.
             client.send_telemetry(id7telemetry) # Upload data to thingsboard.
@@ -134,13 +127,35 @@ while True:
                 ID9_main.id_9_alarm(True) # Run alarm section of ID9_main.py
             else:
                 ID9_main.id_9_alarm(False) # For turning off light and sound from alarm
+                
         if backend.left_button.value()==1 or backend.right_button.value()==1: # See if buttons have changed value, for the blinker
-            if backend.left_button.value()==1: # Check if left button is manipulated
-                print("ID 11 left running") # Troubleshooting
-                backend.left_blinker_func() # Execute function for left signal blinker
-            if backend.right_button.value()==1: # Check if right button
-                print("ID 11 right running") # -||-
-                backend.right_blinker_func() # -||-
+            if ticks_ms() > id11_2 + id11timing2:
+                if backend.left_button.value()==1 and backend.right_button.value()==1:
+                    print("Unlocking")
+                    backend.solenoid.value(1)
+                    gennemsnit = 0
+                    for x in range(2000):
+                        gennemsnit += backend.ina_current()
+                    print("Solenid mA: ", gennemsnit/2000)
+                    print("Locking")
+                    backend.solenoid.value(0)
+                elif backend.left_button.value()==1: # Check if left button is manipulated
+                    print("ID 11 left running") # Troubleshooting
+                    ID11_main.left_blinker_func() # Execute function for left signal blinker
+                elif backend.right_button.value()==1: # Check if right button
+                    print("ID 11 right running") # -||-
+                    ID11_main.right_blinker_func() # -||-
+                id11_2 = ticks_ms()
+                
+        if ticks_ms() > id11 + id11timing:
+            ID11_main.blinker()
+        if ticks_ms() > ged + gedtiming:
+            ged = ticks_ms()
+            gennemsnit = 0
+            for x in range(20):
+                gennemsnit += backend.ina_current()
+            print("mA: ", gennemsnit/20)
     except KeyboardInterrupt:
         client.disconnect() # Disconnecting from ThingsBoard
         reset() # Reset ESP
+
